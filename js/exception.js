88b49ac6f19633a093dc906e821775c08897f5e0
minji4252 <alswl7216@naver.com>
2024-04-12 10:54:02 +0900

    함수

---
+++ js/exception.js
@@ -0,0 +1,23 @@
+const checkInput = () => {
+  let input_elem = document.getElementById("input1");
+  let a = input_elem.value;
+  document.getElementById("show").innerHTML = "";
+
+  try {
+    if (a === "") {
+      throw "비어 있습니다.";
+    }
+    //숫자인지 아닌지
+    if (isNaN(a)) {
+      throw "숫자가 아닙니다.";
+    }
+    a = Number(a);
+    if (a < 0 || a > 9) {
+      throw "0 ~ 9 사이의 숫자가 아닙니다.";
+    }
+  } catch (error) {
+    document.getElementById("show").innerHTML = `오류: ${error}`;
+  } finally {
+    input_elem.value = "";
+  }
+};
