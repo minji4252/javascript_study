67da9496948b118714bff76b7311dd8eb9090fcd
minji4252 <alswl7216@naver.com>
2024-04-15 10:34:30 +0900

    배열의 내장 메서드들

--- js/array.js
+++ js/array.js
@@ -1,5 +1,14 @@
-const animals = ["사자", "호랑이", "사슴", "펭귄", "여우", "도마뱀"];
+const numbers = [1, 2, 3, 4, 5];
 
-const result = animals.slice(1, 4); //시작인덱스, 종료인덱스 사이만 추출
+// let arr = numbers.map(function (num) {
+//   return num * num;
+// });
+let arr = numbers.map((num) => num * num); //리액트 버전
+console.log(arr);
+document.getElementById("show").innerHTML = arr;
 
-console.log(result);
+// map()메서드는 forEach()메서드와 달리 매개변수로 사용되는 함수에서
+// 값을 반환하는 return이 존재
+// map()메서드는 원본의 배열을 그대로 두고 새로운 배열을 생성
+// map()메서드는 매개변수로 설정도니 익명함수를 실행하여
+// 얻어진 요소로 구성된 새로운 배열을 반환
