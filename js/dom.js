44f2d79ddf013b7722a3d3422f2d28e426ab0eae
minji4252 <alswl7216@naver.com>
2024-05-03 11:35:14 +0900

    docs: DOM 정리

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,7 @@
-function changeImg() {
-  document.getElementById("image").src = `images/Image-4.png`;
-}
-function changeSize() {
-  document.getElementById("image").width = "400";
-}
+const elem = document.createElement("p");
+const text = document.createTextNode("변경된 단락");
+
+elem.appendChild(text);
+
+const p1 = document.getElementById("p1");
+document.getElementById("box").replaceChild(elem, p1);
